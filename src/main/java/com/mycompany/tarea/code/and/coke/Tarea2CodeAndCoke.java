6ee26496070d2d870ebd8321781c0a8600d7baa0
dam114 <dam114@aglinfo.local>
2025-01-24 12:04:34 +0100

    Subiendo los primeros cambios

---
+++ src/main/java/com/mycompany/tarea/code/and/coke/Tarea2CodeAndCoke.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea.code.and.coke;
+
+/**
+ *
+ * @author dam114
+ */
+public class Tarea2CodeAndCoke {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
